1bd8cd5270f4c5109c8d846458681d7da443be3f
Sion <sionc140720@gmail.com>
2026-03-09 16:05:52 +0530

    added  health check

--- server/index.ts
+++ server/index.ts
@@ -85,6 +85,10 @@ app.use((req, res, next) => {
     await setupVite(httpServer, app);
   }
 
+  app.get("/api/health", (_req, res) => {
+    res.status(200).send("OK");
+  });
+
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
